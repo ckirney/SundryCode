db8be9c4e8e714d442f21feaf1fb9febfcdbd4ee
Chris Kirney <chris.kirney@canada.ca>
2018-09-20 19:44:09 +0000

    Further developing surface geometry code.

--- concave_to_convex_surfaces.rb
+++ concave_to_convex_surfaces.rb
@@ -4,6 +4,22 @@ class Concave_to_convex_surfaces
       # Surfaces and subsurfaces are not returned since they may not be needed nor wanted.
 
   def self.get_guaranteed_concave_surfaces(surface)
+    # Note that points on surfaces are given counterclockwise when looking at the surface from the opposite direction as
+    # the outward normal (i.e. the outward normal is pointing at you).  I use point_a1, point_a2, point_b1 and point b2
+    # lots.  For this, point_a refers to vectors pointing up.  In this case point_a1 is at the top of the vector and
+    # point_a2 is at the bottom of the vector.  Contrarily, point_b refers to vectors pointing down.  In this case
+    # point_b1 is at the bottom of the vector and point_b2 is at the top.  All of this comes about because I cycle
+    # through the points starting at the 2nd point and and going to the last point.  I count vectors as starting from
+    # the last point and going toward the current point.
+    # See following where P1 through P4 are the points.  When cycling through a is where you start and b is where you
+    # end.  the o is the tip of the outward normal pointing at you.
+    #    P2b------------aP1
+    #     a              b
+    #     |              |
+    #     |      o       |
+    #     |              |
+    #     b              a
+    #     P3a-----------bP4
     surface = [
         {X: -1.73853133146172, y: -1.48909706045603},
         {x: -1.23930351151008, y: -0.929390366029091},
@@ -30,7 +46,8 @@ class Concave_to_convex_surfaces
       # Is this line segment pointing up?  If no, then ignore it and go to the next line segment.
       if surf_verts[i][:y].to_f.round(tol) > surf_verts[i-1][:y].to_f.round(tol)
       # Go through each line segment
-        overlap_seg = []
+        overlap_segs = []
+        final_segs = []
         for j in 1..(surf_verts.length-1)
         # Is the line segment to the left of the current (index i) line segment?  If no, then ignore it and go to the next one.
           if surf_verts[j][:x].to_f.round(tol) < surf_verts[i][:x].to_f.round(tol) and surf_verts[j-1][:x].to_f.round(tol) < surf_verts[i-1][:x].to_f.round(tol)
@@ -40,13 +57,27 @@ class Concave_to_convex_surfaces
             # then ignore it and go to the next line segment.
               overlap_y = line_segment_overlap_y?(surf_verts[i][:y].to_f.round(tol), surf_verts[i-1][:y].to_f.round(tol), surf_verts[j][:y].to_f.round(tol), surf_verts[j-1][:y].to_f.round(tol))
               unless (overlap_y[:overlap_start].nil? || overlap_y[:overlap_end].nil?)
-                overlap_seg << [surf_verts[j][:y].to_f.round(tol), surf_verts[j-1][:y].to_f.round(tol)]
+                overlap_seg = {
+                    point_b1: surf_verts[j],
+                    point_b2: surf_verts[j-1],
+                    overlap_y: overlap_y
+                }
+                overlap_segs << overlap_seg
               end
             end
           end
         end
-        if overlap_seg.length > 0
-          puts "hello"
+        if overlap_segs.length > 1
+          overlap_segs.sort.each do |overlap_seg|
+            for j in 1..(overlap_segs.length-1)
+              if overlap_seg[:overlap_y][:start] <= overlap_segs[j][:point_b2][:y] && overlap_seg[:overlap_y][:start] >= overlap_segs[j][:point_b1][:y]
+                puts "hello"
+              end
+              if overlap_seg[:overlap_y][:end] <= overlap_segs[j][:point_b2][:y] && overlap_seg[:overlap_y][:end] >= overlap_segs[j][:point_b1][:y]
+                puts "hello"
+              end
+            end
+          end
         end
       end
     end
