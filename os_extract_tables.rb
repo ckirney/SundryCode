0ffa1c97af4cd475f32c8500426e39fe8a51ccc2
ckirney <chris.kirney@canada.ca>
2018-10-04 20:10:48 +0000

    Extract a managable amount of data from simulations.json.

--- os_extract_tables.rb
+++ os_extract_tables.rb
@@ -5,18 +5,13 @@ class OS_table_extract
   in_json_file = "./simulations.json"
   # Name of file you want this script to produce:
   out_json_file = in_json_file[0, (in_json_file.length - 5)] + "_out.json"
-  output_array = []
-  tbl_index = 0
   output_array2 = []
-  trackindex = 0
-  build_test = Array.new(16, 0)
-  weather_locs = ["nothing", "no_building", 100000]
-#Z  output_array.push(weather_locs)
+  count = 0
   file = File.read(in_json_file)
   data_tables = JSON.parse(file)
   data_tables.each do |data_table|
     count += 1
-    if count < 100
+    if count < 10
       output_array2 << data_table
     end
   end
