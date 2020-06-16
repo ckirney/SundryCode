a18257a3c892658c9075551765752da05c542ff8
Chris Kirney <chris.kirney@canada.ca>
2020-06-16 17:24:50 +0000

    Including pump efficiencies json to xlsx script.

---
+++ json_to_xlsx/pump_efficiencies_to_xlsx.rb
@@ -0,0 +1,47 @@
+require 'write_xlsx'
+require 'json'
+require 'fileutils'
+
+col_titles = [
+    "Pump Ues",
+    "Total Pump Efficiency (including motor)"
+]
+
+json_titles = [
+    "loop_type",
+    "comb_eff"
+]
+
+in_file = './data/pump_efficiencies.json'
+json_info = JSON.parse(File.read(in_file))
+table_info = json_info["tables"]["pump_combined_eff"]["table"]
+out_file = './' + in_file[7..-5] + 'xlsx'
+workbook = WriteXLSX.new(out_file)
+worksheet = workbook.add_worksheet
+col = row = 0
+col_titles.each do |col_title|
+  worksheet.write(row, col, col_title)
+  col += 1
+end
+row += 1
+table_info.each do |table_entry|
+  col = 0
+  json_titles.each do |json_title|
+    xlsx_output = table_entry[json_title]
+    if xlsx_output == '-'
+      xlsx_output = 0
+    elsif xlsx_output.nil?
+      if json_title.include?('coeff')
+        xlsx_output = 0
+      else
+        xlsx_output = '-'
+      end
+    else
+      xlsx_output.is_a?(Float) ? xlsx_output.to_f : xlsx_output.to_s
+    end
+    worksheet.write(row, col, xlsx_output)
+    col += 1
+  end
+  row += 1
+end
+workbook.close
