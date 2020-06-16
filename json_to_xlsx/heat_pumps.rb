578484c5e582b84cf57f67eb21c9febe903aad7b
Chris Kirney <chris.kirney@canada.ca>
2020-06-16 14:30:30 +0000

    Including Heating Type as an output and fixing references.

--- json_to_xlsx/heat_pumps.rb
+++ json_to_xlsx/heat_pumps.rb
@@ -4,21 +4,23 @@ require 'fileutils'
 
 col_titles = [
     "Heat Pump Type",
+    "Heating Type",
     "Minimum Capacity (BTU/hr)",
     "Maximum Capacity (BTU/hr)",
     "Energy Efficiency Ratio"
 ]
 
 json_titles = [
-    "fuel_type",
+    "subcategory",
+    "heating_type",
     "minimum_capacity",
     "maximum_capacity",
     "minimum_full_load_efficiency"
 ]
 
-in_file = './data/boilers.json'
+in_file = './data/heat_pumps.json'
 json_info = JSON.parse(File.read(in_file))
-table_info = json_info["tables"]["boilers"]["table"]
+table_info = json_info["tables"]["heat_pumps"]["table"]
 out_file = './' + in_file[7..-5] + 'xlsx'
 workbook = WriteXLSX.new(out_file)
 worksheet = workbook.add_worksheet
