12da23f700f3579b0329472110ea9e0a2117a004
Chris Kirney <chris.kirney@canada.ca>
2020-06-16 16:10:02 +0000

    Adding fan json to xlsx script.

---
+++ json_to_xlsx/fan_power_to_xlsx.rb
@@ -0,0 +1,47 @@
+require 'write_xlsx'
+require 'json'
+require 'fileutils'
+
+col_titles = [
+    "Fan Type",
+    "Fan Total Efficiency"
+]
+
+json_titles = [
+    "fan_type",
+    "fan_total_efficiency"
+]
+
+in_file = './data/fans.json'
+json_info = JSON.parse(File.read(in_file))
+table_info = json_info["tables"]["fans"]["table"]
+out_file = './' + in_file[7..-5] + 'xlsx'
+workbook = WriteXLSX.new(out_file)
+worksheet = workbook.add_worksheet
+col = row = 0
+col_titles.each do |col_title|
+  worksheet.write(row, col, col_title)
+  col += 1
+end
+row += 1
+table_info.each do |table_entry|
+  col = 0
+  json_titles.each do |json_title|
+    xlsx_output = table_entry[json_title]
+    if xlsx_output == '-'
+      xlsx_output = 0
+    elsif xlsx_output.nil?
+      if json_title.include?('coeff')
+        xlsx_output = 0
+      else
+        xlsx_output = '-'
+      end
+    else
+      xlsx_output.is_a?(Float) ? xlsx_output.to_f : xlsx_output.to_s
+    end
+    worksheet.write(row, col, xlsx_output)
+    col += 1
+  end
+  row += 1
+end
+workbook.close
