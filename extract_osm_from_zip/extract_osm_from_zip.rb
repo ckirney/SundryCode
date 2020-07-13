43356d360a9bc6ff4427b142fa47a104daf250e4
Chris Kirney <chris.kirney@canada.ca>
2020-07-13 20:03:53 +0000

    Correcting how zip osm is saved.

--- extract_osm_from_zip/extract_osm_from_zip.rb
+++ extract_osm_from_zip/extract_osm_from_zip.rb
@@ -28,14 +28,22 @@ def unzip_files(zip_name:, search_name: nil)
   return output
 end
 
+def unzip_osm(zip_name:, search_name:, out_loc:)
+  Zip::File.open(zip_name) do |zip_file|
+    zip_file.each do |entry|
+      if entry.name == search_name
+        entry.extract(out_loc)
+      end
+    end
+  end
+end
+
 zip_files = Dir["./files/*.zip"]
 zip_files.each do |zip_file|
   qaqc_raw = unzip_files(zip_name: zip_file, search_name: "qaqc.json")
   qaqc_info = JSON.parse(qaqc_raw[:out_info][0])
-  in_osm_info = unzip_files(zip_name: zip_file, search_name: "in.osm")
-  in_osm = in_osm_info[:out_info][0]
   heat_source = qaqc_info["building"]["principal_heating_source"].gsub(" ", "")
   city_loc = qaqc_info["geography"]["city"].gsub(" ", "") + "_" + qaqc_info["geography"]["state_province_region"]
-  out_osm_name = './out_osm/' + qaqc_info["building_type"] + "_" + qaqc_info["template"] + "_" + heat_source + "_" + city_loc + ".osm"
-  File.write(out_osm_name, in_osm_info)
+  out_osm_name = '/home/osdev/SundryCode/extract_osm_from_zip/out_osm/' + qaqc_info["building_type"] + "_" + qaqc_info["template"] + "_" + heat_source + "_" + city_loc + ".osm"
+  unzip_osm(zip_name: zip_file, search_name: "in.osm", out_loc: out_osm_name)
 end
