d9cba0b2bce198c8b49003973408243613f909a4
Chris Kirney <chris.kirney@canada.ca>
2020-07-30 17:34:33 +0000

    Including write_xlsx in Gemfile and adding json code to get_osm_info.rb.

--- results_geo_analysis/get_osm_info.rb
+++ results_geo_analysis/get_osm_info.rb
@@ -3,13 +3,18 @@ require '/usr/local/openstudio-3.0.1/Ruby/openstudio'
 require 'fileutils'
 require 'json'
 require 'csv'
+require 'write_xlsx'
 
 cost_files = Dir['/home/osdev/SundryCode/results_geo_analysis/results_files/**/*.osm']
 total_out = []
+airloops_out = []
+total_out_json = []
 cost_files.each do |cost_file|
-  #json_cont = JSON.parse(File.read(cost_file.to_s))
+  json_file = cost_file[0..-4] + 'json'
+  json_cont = JSON.parse(File.read(json_file))
   model = BTAP::FileIO.load_osm(cost_file)
   total_out << cost_file.to_s
+  tz_json = []
   model.getThermalZones.sort.each do |tz|
     ext_wallarea = 0
     floor_area = 0
@@ -120,9 +125,87 @@ cost_files.each do |cost_file|
         ventilation_air*1000,
         (ventilation_air/tz_floor_area)
     ]
+    tz_json << {
+        tz_name: tz_out[0],
+        ext_wall_area_m2: tz_out[1],
+        floor_area_m2: tz_out[2],
+        tz_floor_area_m2: tz_out[3],
+        volume_m3: tz_out[4],
+        exp_floor_area_m2: tz_out[5],
+        ground_floor_area_m2: tz_out[6],
+        ground_wall_area_m2: tz_out[7],
+        door_area_m2: tz_out[8],
+        dome_area_m2: tz_out[9],
+        wall_subsurf_area_m2: tz_out[10],
+        skylight_area_m2: tz_out[11],
+        roof_area_m2: tz_out[12],
+        window_area_m2: tz_out[13],
+        num_people: tz_out[14],
+        area_people_m2_per_person: tz_out[15],
+        light_power_W: tz_out[16],
+        light_power_W_per_m2: tz_out[17],
+        electric_power_W: tz_out[18],
+        electric_power_W_per_m2: tz_out[19],
+        shw_m3_per_s: tz_out[20],
+        shw_L_per_hour_per_person: tz_out[21],
+        ventilation_air_L_per_s: tz_out[22],
+        ventilation_air_m3_per_s_per_m2: tz_out[23]
+    }
     total_out << tz_out
   end
+  total_out_json << {
+      file_name: cost_file,
+      tz_info: tz_json
+  }
+  airloops_out << json_file
+  airloops_fileout = []
+  json_cont["air_loops"].each do |air_loop|
+    al_name = air_loop["name"].to_s
+    heating_coils = air_loop["heating_coils"]["coil_heating_gas"] unless air_loop["heating_coils"]["coil_heating_gas"].empty?
+    heating_coils = air_loop["heating_coils"]["coil_heating_electric"] unless air_loop["heating_coils"]["coil_heating_electric"].empty?
+    if air_loop["supply_fan"].nil?
+      supply_fan_motor_eff = "none"
+      supply_fan_eff = "none"
+      supply_fan_prise = "none"
+    else
+      supply_fan_motor_eff = air_loop["supply_fan"]["motor_efficiency"]
+      supply_fan_eff = air_loop["supply_fan"]["fan_efficiency"]
+      supply_fan_prise = air_loop["supply_fan"]["pressire_rise"]
+    end
+    if air_loop["return_fan"].nil?
+      return_fan_motor_eff = "none"
+      return_fan_eff = "none"
+      return_fan_prise = "none"
+    else
+      return_fan_motor_eff = air_loop["return_fan"]["motor_efficiency"]
+      return_fan_eff = air_loop["return_fan"]["fan_efficiency"]
+      return_fan_prise = air_loop["return_fan"]["pressire_rise"]
+    end
+    air_loop["economizer"].nil? ? economizer = "none" : economizer = air_loop["economizer"]["control_type"]
+    ind_out = {
+        airloop_name: al_name,
+        type: al_name[0..4],
+        heating_coil: heating_coils,
+        cooling_coil_dx: air_loop["cooling_coils"]["dx_single_speed"],
+        cooling_coils_water: air_loop["cooling_coils"]["coil_cooling_water"],
+        area_served: air_loop["total_floor_area_served"],
+        outdoor_air: air_loop["outdoor_air_L_per_s"],
+        supply_fan_motor_eff: supply_fan_motor_eff,
+        supply_fan_eff: supply_fan_eff,
+        supply_fan_prise: supply_fan_prise,
+        return_fan_motor_eff: return_fan_motor_eff,
+        return_fan_eff: return_fan_eff,
+        return_fan_prise: return_fan_prise,
+        economizer: economizer
+    }
+    airloops_fileout << ind_out
+  end
+  airloops_out << {
+      file_name: json_file,
+      airloops: airloops_fileout
+  }
 end
+
 CSV.open('./results_tz_geo.csv', "w") do |csv|
   csv << [
       "Thermal_Zone_Name",
@@ -161,3 +244,30 @@ CSV.open('./results_tz_geo.csv', "w") do |csv|
   end
 end
 
+in_file = './data/boilers.json'
+json_info = JSON.parse(File.read(in_file))
+table_info = json_info["tables"]["boilers"]["table"]
+out_file = './' + in_file[7..-5] + 'xlsx'
+workbook = WriteXLSX.new(out_file)
+worksheet = workbook.add_worksheet
+col = row = 0
+col_titles.each do |col_title|
+  worksheet.write(row, col, col_title)
+  col += 1
+end
+row += 1
+table_info.each do |table_entry|
+  col = 0
+  json_titles.each do |json_title|
+    xlsx_output = table_entry[json_title]
+    if xlsx_output == '-'
+      xlsx_output = 0
+    else
+      xlsx_output.is_a?(Float) ? xlsx_output.to_f : xlsx_output.to_s
+    end
+    worksheet.write(row, col, xlsx_output)
+    col += 1
+  end
+  row += 1
+end
+workbook.close
