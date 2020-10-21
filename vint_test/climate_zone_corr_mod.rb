2e09d28fbd5bb82997d54314c3b9b804561df03e
Chris Kirney <chris.kirney@canada.ca>
2020-10-21 19:56:28 +0000

    Including provincial information in population data and including method to create climate zone by population.

---
+++ vint_test/climate_zone_corr_mod.rb
@@ -0,0 +1,34 @@
+require 'fileutils'
+require 'json'
+require 'csv'
+
+in_csv = "./census_hdd_out_mod.csv"
+out_json = "./climate_zone_pop_frac.json"
+in_data = CSV.read(in_csv)
+out_array = []
+tot_pop = 0
+for i in (2000..7000).step(1000)
+  top_lim = i + 999
+  if i == 7000
+    top_lim = 99999
+  end
+  clim_zone_info = in_data.select{|row| (row[6].to_f <= top_lim) && (row[6].to_f >= i)}
+  if clim_zone_info.empty?
+    next
+  end
+  pop = 0
+  clim_zone_info.each do |city|
+    pop += city[0].to_f
+  end
+  out_array << {
+      min_hdd18: i,
+      max_hdd18: top_lim,
+      population: pop,
+      pop_frac: 0
+  }
+  tot_pop += pop
+end
+out_array.each do |clm_zone|
+  clm_zone[:pop_frac] = clm_zone[:population]/tot_pop
+end
+File.write(out_json, JSON.pretty_generate(out_array))
