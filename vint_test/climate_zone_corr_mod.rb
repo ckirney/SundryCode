eb32e622ad99fd60f8394ae4d87014ae96c98879
Chris Kirney <chris.kirney@canada.ca>
2020-10-21 21:01:21 +0000

    Changing format again, again.  Also, updating province climate zone code.

--- vint_test/climate_zone_corr_mod.rb
+++ vint_test/climate_zone_corr_mod.rb
@@ -24,28 +24,46 @@ in_data = CSV.read(in_csv)
 out_array = []
 tot_pop = 0
 
-for i in (2000..7000).step(1000)
-  top_lim = i + 999
-  if i == 7000
-    top_lim = 99999
+provinces.each do |province|
+  prov_pop = 0
+  prov_clim = []
+  prov_data = in_data.select{|data_row| data_row.to_s == province}
+  for i in (2000..7000).step(1000)
+    top_lim = i + 999
+    if i == 7000
+      top_lim = 99999
+    end
+    clim_zone_info = prov_data.select{|row| (row[6].to_f <= top_lim) && (row[6].to_f >= i)}
+    if clim_zone_info.empty?
+      next
+    end
+    pop = 0
+    clim_zone_info.each do |city|
+      pop += city[0].to_f
+    end
+
+    prov_clim << {
+        min_hdd18: i,
+        max_hdd18: top_lim,
+        population: pop,
+        pop_frac: 0
+    }
+    tot_pop += pop
+    prov_pop += pop
   end
-  clim_zone_info = in_data.select{|row| (row[6].to_f <= top_lim) && (row[6].to_f >= i)}
-  if clim_zone_info.empty?
-    next
-  end
-  pop = 0
-  clim_zone_info.each do |city|
-    pop += city[0].to_f
+  prov_clim.each do |clm_zone|
+    clm_zone[:prov_pop_frac] = clm_zone[:population]/prov_pop
   end
   out_array << {
-      min_hdd18: i,
-      max_hdd18: top_lim,
-      population: pop,
-      pop_frac: 0
+      province: province,
+      prov_pop: prov_pop,
+      prov_climate_zones: prov_clim
   }
-  tot_pop += pop
 end
-out_array.each do |clm_zone|
-  clm_zone[:pop_frac] = clm_zone[:population]/tot_pop
+
+out_array.each do |prov|
+  prov.each do |prov_cz|
+    prov_cz[:nat_pop_frac] = prov_cz[:population].to_f/tot_pop.to_f
+  end
 end
 File.write(out_json, JSON.pretty_generate(out_array))
